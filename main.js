85efc24634a4ff01e7d82e8e02662865f2667cde
Mike Taslakjian <mike.taslakjian.techx@gmail.com>
2025-11-25 16:04:30 +0400

    Added scrolling functionality for search buttons

--- main.js
+++ main.js
@@ -1,11 +1,36 @@
 import { getMovies, showCards } from "./Helpers.js";
 
+// Variables
+
 const movieCards = document.querySelector(".movie-cards");
 const select = document.querySelector(".selection select");
+const title = document.querySelector(".title");
+const searchBtn = document.querySelector(".search-button");
+const bigButton = document.querySelector(".big-button");
 
-console.log(select);
+const observer = new IntersectionObserver((entries, observer) => {
+    const button = entries[0];
+    console.log(button);
+
+    if (button.intersectionRect.top <= 0) {
+        bigButton.style.display = bigButton.style.display === "block" ? "none" : "block";
+    }
+});
+
+observer.observe(searchBtn);
+
+//Event Listeners
 
 window.addEventListener("DOMContentLoaded", async () => {
     const movies = await getMovies(select.value);
     movieCards.innerHTML = showCards(movies);
+});
+
+title.addEventListener("click", () => {
+    const selection = document.querySelector(".selection");
+    selection.style.display = selection.style.display === "block" ? "none" : "block";
+});
+
+searchBtn.addEventListener("click", () => {
+
 });
