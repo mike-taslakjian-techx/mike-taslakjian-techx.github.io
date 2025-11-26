a12079b936c59f1d3cd8fecafb667fa71b8cf575
Mike Taslakjian <mike.taslakjian.techx@gmail.com>
2025-11-26 16:07:09 +0400

    Implemented genre filter functionality

--- Helpers.js
+++ Helpers.js
@@ -1,11 +1,16 @@
 // Function accepts the sort_by parameter and returns a sorted array of movies
 
-async function getMovies (sortBy, pages) {
-    const url = `https://api.themoviedb.org/3/movie/popular?page=${pages}&sort_by=${sortBy}`;
-    console.log(url);
+async function getMovies (sortBy, pages, genres) {
+    let url = "";
+    if (genres.length === 0) {
+        url = `https://api.themoviedb.org/3/discover/movie?page=${pages}&sort_by=${sortBy}`;
+    } else {
+        url = `https://api.themoviedb.org/3/discover/movie?page=${pages}&sort_by=${sortBy}&with_genres=${genres.join(",")}`;
+    }
+    
     try {
         const response = await fetch(url, {
-            method: "GET",
+            method: "GET",  
             headers: {
                 accept: 'application/json',
                 Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyM2NmZWM3NmFkZDYzNWI4NjQ1MDdmMDQxMmY4YTg2OSIsIm5iZiI6MTc2Mzk3NzIwMi40NDgsInN1YiI6IjY5MjQyN2YyZDMyZjYzYWFiYWEzYjhjMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.xqZteU0a8xJQ7ePBy4DlcVHwv3qAzKyoG1uqKdKokME"
@@ -28,11 +33,12 @@ async function getMovies (sortBy, pages) {
 
 function showCards (movies) {
     const moviesHTML = movies.map(({ poster_path, title, release_date }) => {
+        const src = poster_path ? `https://media.themoviedb.org/t/p/w220_and_h330_face${poster_path}` : "assets/cards/no-image.svg";
         return (
             `<div class="movie-card">
                 <button><span>...</span></button>
                 <div></div>
-                <a href=""><img src=https://media.themoviedb.org/t/p/w220_and_h330_face/${poster_path} alt="${title} poster" /><a/>
+                <a href=""><img src="${src}" alt="${title} poster" /><a/>
                 <div class="movie-info">
                     <p class="movie-title">${title}</p>
                     <p class="release-date">${release_date}</p>
@@ -44,4 +50,32 @@ function showCards (movies) {
     return moviesHTML.join("");
 };
 
-export { getMovies, showCards };
+// Function to fetch all the genres with their respective IDs
+
+async function getGenres () {
+    try {
+        const response = await fetch("https://api.themoviedb.org/3/genre/movie/list", {
+            method: "GET",
+            headers: {
+                    accept: 'application/json',
+                    Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyM2NmZWM3NmFkZDYzNWI4NjQ1MDdmMDQxMmY4YTg2OSIsIm5iZiI6MTc2Mzk3NzIwMi40NDgsInN1YiI6IjY5MjQyN2YyZDMyZjYzYWFiYWEzYjhjMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.xqZteU0a8xJQ7ePBy4DlcVHwv3qAzKyoG1uqKdKokME"
+            }
+        });
+        if (!response.ok) {
+            console.log("Oops! Something went wrong...");
+        }
+        const data = await response.json();
+        return data.genres;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+function activateBtn (button) {
+    button.style.backgroundColor = "var(--blue)";
+    button.style.color = "var(--white)";
+    button.disabled = false;
+    button.style.cursor = "pointer";
+}
+
+export { getMovies, showCards, getGenres, activateBtn };
--- main.js
+++ main.js
@@ -1,14 +1,16 @@
-import { getMovies, showCards } from "./Helpers.js";
+import { getMovies, showCards, getGenres, activateBtn } from "./Helpers.js";
 
 // Variables
 
 const movieCards = document.querySelector(".movie-cards");
 const select = document.querySelector(".selection select");
-const title = document.querySelector(".title");
+const titles = document.querySelectorAll(".title");
 const searchBtn = document.querySelector(".search-button");
 const bigButton = document.querySelector(".big-button");
 const loadMoreBtn = document.querySelector(".load-more");
+const genres = document.querySelector(".genres");
 let page = 1;
+let genreIDs = [];
 
 const observer = new IntersectionObserver((entries, _observer) => {
     const button = entries[0];
@@ -23,41 +25,68 @@ observer.observe(searchBtn);
 //Event Listeners
 
 window.addEventListener("DOMContentLoaded", async () => {
-    const movies = await getMovies(select.value, page);
+    const movies = await getMovies(select.value, page, genreIDs);
+    const genresArr = await getGenres();
     movieCards.innerHTML = showCards(movies);
+    
+    genresArr.forEach(({ id, name }) => genres.innerHTML += `<button id="${id}">${name}</button>`)
+    
 });
 
 select.addEventListener("change", () => {
-    searchBtn.disabled = false;
-    searchBtn.classList.add("button-active");
+    activateBtn(searchBtn);
 });
 
-title.addEventListener("click", () => {
-    const selection = document.querySelector(".selection");
-    selection.style.display = selection.style.display === "block" ? "none" : "block";
-});
+titles.forEach((title) => {
+    title.addEventListener("click", (e) => {
+        const span = e.target.querySelector("span");
+        span.style.transform = span.style.transform === "rotate(90deg)" ? "rotate(0)" : "rotate(90deg)";
+        let selection = null;
+
+        if (e.target.id === "sort-menu") {
+            selection = document.querySelector(".sort .selection");
+        }
+
+        if (e.target.id === "filter-menu") {
+            selection = document.querySelector(".filter .selection");
+        }
+
+        selection.style.display = selection.style.display === "block" ? "none" : "block";
+    })
+})
 
 searchBtn.addEventListener("click", async () => {
     page = 1;
     movieCards.innerHTML = "";
-    const movies = await getMovies(select.value, page);
-    console.log(movies);
+    const movies = await getMovies(select.value, page, genreIDs);
     movieCards.innerHTML = showCards(movies);
     searchBtn.disabled = true;
+    searchBtn.style.cursor = "auto";
     searchBtn.classList.remove("button-active");
+    bigButton.style.display = "none";
+    genreIDs = [];
 });
 
 bigButton.addEventListener("click", async () => {
     page = 1;
     movieCards.innerHTML = "";
-    const movies = await getMovies(select.value, page);
+    const movies = await getMovies(select.value, page, genreIDs);
     movieCards.innerHTML = showCards(movies);
-    console.log(movies);
     searchBtn.disabled = true;
+    searchBtn.style.cursor = "auto";
+    bigButton.style.display = "none";
+    genreIDs = [];
 });
 
 loadMoreBtn.addEventListener("click", async () => {
     page++;
-    const movies = await getMovies(select.value, page);
+    const movies = await getMovies(select.value, page, genreIDs);
     movieCards.innerHTML += showCards(movies);
+});
+
+genres.addEventListener("click", (e) => {
+    const button = e.target.closest("button");
+    genreIDs.push(button.id);
+    activateBtn(searchBtn);
+    activateBtn(button);
 });
